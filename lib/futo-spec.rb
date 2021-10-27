d0f01d91c83f6227f78d9191658edb4b3a200ddf
Sean Felipe <sean@addlightness.tech>
2021-10-27 00:16:17 -0400

    didnt work

--- lib/futo-spec.rb
+++ lib/futo-spec.rb
@@ -486,6 +486,12 @@ class FutoSpec
   def run_commands_in_block_context(bullet)
     bullet.associated_commands.each do |cmd|
       begin
+=begin
+        Whirly.start do
+          pa "  #{cmd}", COLORS[:support] if cmd != 'breakpoint'
+          binding = eval(cmd, binding)
+        end
+=end
         binding = Whirly.start { eval(cmd, binding) }
       rescue RSpec::Expectations::ExpectationNotMetError => e
         pa e, COLORS[:error], :bright
